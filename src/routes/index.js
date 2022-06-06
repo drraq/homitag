f875152a4aeb420c18d34c644f6e9b2362e733cd
drraq <diracist@gmail.com>
2022-06-06 16:56:59 +0500

    default route help added

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,116 @@ module.exports = async (app, container) => {
         res.json({
             success: false,
             data: {
-                message: "[404] Route not found"
+                message: "[404] Route not found",
+                help: {
+                    flow: {
+                        step1: "Register a user using /auth/register API",
+                        step2: "Acquire JSON Web Token (JWT) using /auth/login API",
+                        step3: "Send 'JWT token' along every request for Genre and Movie"
+                    },
+                    api: {
+                        auth:[
+                            {
+                                description: "To register a user",
+                                method: 'POST',
+                                endpoint: "/auth/register",
+                                body: {
+                                    name: "<string>",
+                                    email: "<string>",
+                                    password: "<string>"
+                                }
+                            },
+                            {
+                                description: "To login a user",
+                                method: 'POST',
+                                endpoint: "/auth/login",
+                                body: {
+                                    email: "<string>",
+                                    password: "<string>"
+                                }
+                            }],
+                        genres:[
+                            {
+                                description: "To list all genres",
+                                method: "GET",
+                                endpoint: "/genres",
+                                query: {
+                                    page: "<integer>",
+                                    pageSize: "<integer>",
+                                    sortBy: "<string>",
+                                    sortIn: "(1 | -1 )"
+                                }
+                            },
+                            {
+                                description: "To add a genre",
+                                method: "POST",
+                                endpoint: "/genres/add",
+                                body: {
+                                    name: "<string>",
+                                    description: "<string>"
+                                }
+                            },
+                            {
+                                description: "To delete a genre",
+                                method: "DELETE",
+                                endpoint: "/genres/delete/:id",
+                                path: {
+                                    id: "<string[24]>"
+                                }
+                            },
+                            {
+                                description: "To show details of a genre",
+                                method: "GET",
+                                endpoint: "/genres/show/:id",
+                                path: {
+                                    id: "<string[24]>"
+                                }
+                            }
+                        ],
+                        movies:[
+                            {
+                                description: "To list all movies",
+                                method: "GET",
+                                endpoint: "/movies",
+                                query: {
+                                    page: "<integer>",
+                                    pageSize: "<integer>",
+                                    sortBy: "<string>",
+                                    sortIn: "(1 | -1 )"
+                                }
+                            },
+                            {
+                                description: "To add a movie",
+                                method: "POST",
+                                endpoint: "/movies/add",
+                                body: {
+                                    name: "<string>",
+                                    description: "<string>",
+                                    genre: "<string>",
+                                    duration: "<integer[minutes]>",
+                                    rating: "<integer>",
+                                    releaseDate: "<ISO Date>",
+                                }
+                            },
+                            {
+                                description: "To delete a movie",
+                                method: "DELETE",
+                                endpoint: "/movies/delete/:id",
+                                path: {
+                                    id: "<string[24]>"
+                                }
+                            },
+                            {
+                                description: "To show details of a movie",
+                                method: "GET",
+                                endpoint: "/movies/show/:id",
+                                path: {
+                                    id: "<string[24]>"
+                                }
+                            }
+                        ]
+                    }
+                }
             }
         })
     })
